adcc6e15405ab61850f125e77eb7db3f71c93a3f
Andrei Boldyrev <andrei.boldyrev@purrweb.com>
2022-12-07 19:45:31 +0600

    chore: commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+module.exports = {extends: ['@commitlint/config-conventional']};
+
+/*
+type value
+[
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test'
+];
+*/
